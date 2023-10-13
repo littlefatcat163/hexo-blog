f8e9b689a6b7451ba3badca67a1fa4359bff64b9
littlefatcat <littlefatcat@163.com>
2023-10-13 18:18:21 +0800

    feat(1.1.0): setup and move house file

---
+++ scripts/injectorIntercept.js
@@ -0,0 +1,20 @@
+const { name, version } = require('../package.json');
+
+const __myGlobal = {
+    name,
+    version,
+    NAME_UP: name.toUpperCase()
+};
+
+const __globalScript = `<script>var __global = ${JSON.stringify(__myGlobal)}; console.info('__global', __global); </script>`;
+
+const getScript = (url) => {
+    return `<script src="${url}"></script>`;
+}
+
+hexo.extend.injector.register('head_begin', __globalScript, 'default');
+hexo.extend.injector.register('head_begin', getScript(`/${name}/js/authorization.js`), 'default');
+
+hexo.extend.filter.register('theme_inject', function(injects) {
+    injects.footer.raw('default', `<div class="footer-inner"><div class="footer-content"><a href="https://hexo.io" target="_blank" rel="nofollow noopener"><span>Hexo</span> <i class="iconfont icon-love"></i> ${version}</a></div></div>`);
+});
