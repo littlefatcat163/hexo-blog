cd83e39e511e3e7e444e01340a58f567c2d03934
littlefatcat <littlefatcat@163.com>
2023-11-10 23:27:46 +0800

    feat(package): sync theme 1.1.0

--- scripts/injectorIntercept.js
+++ scripts/injectorIntercept.js
@@ -14,6 +14,7 @@ const getScript = (url) => {
 
 hexo.extend.injector.register('head_begin', __globalScript, 'default');
 hexo.extend.injector.register('head_begin', getScript(`/${name}/js/authorization.js`), 'default');
+hexo.extend.injector.register('head_begin', `<link rel="stylesheet" href="/${name}/css/custom.css" />`, 'default');
 
 hexo.extend.filter.register('theme_inject', function(injects) {
     injects.footer.raw('default', `<div class="footer-inner"><div class="footer-content"><a href="https://hexo.io" target="_blank" rel="nofollow noopener"><span>Hexo</span> <i class="iconfont icon-love"></i> ${version}</a></div></div>`);
