7952c9bc9915bd12244d9d0f14107bed39042010
littlefatcat <littlefatcat@163.com>
2024-03-27 16:42:07 +0800

    feat(ppackage): remove verify

--- scripts/injectorIntercept.js
+++ scripts/injectorIntercept.js
@@ -1,19 +1,19 @@
 const { name, version } = require('../package.json');
 
-const __myGlobal = {
-    name,
-    version,
-    NAME_UP: name.toUpperCase()
-};
+// const __myGlobal = {
+//     name,
+//     version,
+//     NAME_UP: name.toUpperCase()
+// };
 
-const __globalScript = `<script>var __global = ${JSON.stringify(__myGlobal)}; console.info('__global', __global); </script>`;
+// const __globalScript = `<script>var __global = ${JSON.stringify(__myGlobal)}; console.info('__global', __global); </script>`;
 
-const getScript = (url) => {
-    return `<script src="${url}"></script>`;
-}
+// const getScript = (url) => {
+//     return `<script src="${url}"></script>`;
+// }
 
-hexo.extend.injector.register('head_begin', __globalScript, 'default');
-hexo.extend.injector.register('head_begin', getScript(`/${name}/js/authorization.js`), 'default');
+// hexo.extend.injector.register('head_begin', __globalScript, 'default');
+// hexo.extend.injector.register('head_begin', getScript(`/${name}/js/authorization.js`), 'default');
 hexo.extend.injector.register('head_begin', `<link rel="stylesheet" href="/${name}/css/custom.css" />`, 'default');
 
 /* hexo.extend.filter.register('theme_inject', function(injects) {
